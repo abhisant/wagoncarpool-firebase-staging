5945e20656f52b1794bafccf2083edb638099825
Abhishek Sant <abhisheksant@abhishekslaptop.lan>
2024-04-16 22:09:45 -0700

    Fix blog

--- dist/assets/swipe-back-legacy-b3f7a9d9.js
+++ dist/assets/swipe-back-legacy-b3f7a9d9.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-3f58d2d1.js"],(function(t,e){"use strict";var n,r,i;return{setters:[function(t){n=t.i,r=t.a,i=t.b}],execute:function(){
+System.register(["./index-legacy-0048c575.js"],(function(t,e){"use strict";var n,r,i;return{setters:[function(t){n=t.i,r=t.a,i=t.b}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
