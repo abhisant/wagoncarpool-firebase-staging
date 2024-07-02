2e3fe3c8eda54758aff57bd259a78cf6424f4e37
Abhishek Sant <abhisheksant@abhishekslaptop.lan>
2024-07-02 14:40:39 -0700

    Add UI

--- dist/assets/md.transition-legacy-006d4d60.js
+++ dist/assets/md.transition-legacy-006d4d60.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-5870a2b2.js"],(function(n,e){"use strict";var t,i;return{setters:[function(n){t=n.z,i=n.y}],execute:function(){
+System.register(["./index-legacy-76f25717.js"],(function(n,e){"use strict";var t,i;return{setters:[function(n){t=n.z,i=n.y}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
--- dist/assets/swipe-back-legacy-92930477.js
+++ dist/assets/swipe-back-legacy-92930477.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-5870a2b2.js"],(function(t,e){"use strict";var n,r,i;return{setters:[function(t){n=t.i,r=t.a,i=t.b}],execute:function(){
+System.register(["./index-legacy-76f25717.js"],(function(t,e){"use strict";var n,r,i;return{setters:[function(t){n=t.i,r=t.a,i=t.b}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
